fbb01c46eb8d906b5bf12a9e6a1f0d0d1aba0a55
Jordan Caley <jordancaley10@gmail.com>
2021-06-07 19:32:10 +0200

    Update profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -38,7 +38,39 @@ componentDidMount() {
 render() {
     return (
       <div>
-        <h1>Welcome!</h1>
+        <h1>Welcome, {this.state.user.firstName}!</h1>
+        <h2>Your profile:</h2>
+        <div className="row row-cols-1 row-cols-md-6 g-4 p-2">
+            <div className="col">      
+              <div className="card h-100 ml-1">
+                <img className="card-img-top" src={this.state.user.profileImg} alt={this.state.user.firstName} />
+                <div className="card-body">
+                  <h5 className="card-title">{this.state.user.firstName} {this.state.user.lastName}</h5>
+                  <p className="card-text">Email: {this.state.user.email}</p>
+                  <p className="card-text">Phone: {this.state.user.phoneNumber}</p>
+                  <p className="card-text">Description: {this.state.user.description}</p>
+                  <button type="button" className="btn btn-warning mx-3">Edit</button>
+                </div>
+              </div>
+            </div>
+        </div>
+        <h2>Your services:</h2>
+        <div className="row row-cols-1 row-cols-md-6 g-4 p-2">
+            {this.state.userServices.map(service =>   
+            <div className="col" key={service._id}>      
+              <div className="card h-100 ml-1">
+                <img className="card-img-top" src={service.images[0]} alt={service.name} />
+                <div className="card-body">
+                  <h5 className="card-title">{service.name}</h5>
+                  <p className="card-text">{service.cityName}</p>
+                  <button type="button" className="btn btn-warning mx-3">Edit</button>
+                  <button type="button" className="btn btn-danger">Delete</button>
+                </div>
+              </div>
+            </div>
+            )}
+        </div>
+        <button type="button" class="btn btn-primary mx-3">Add a service</button>
       </div>
     );
   }
