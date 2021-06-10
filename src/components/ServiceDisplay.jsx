7ff80135c5dd3d3de4ad4f75b80b76c4898ee513
Jordan Caley <jordancaley10@gmail.com>
2021-06-10 14:11:10 +0200

    Update map pop up card

--- src/components/ServiceDisplay.jsx
+++ src/components/ServiceDisplay.jsx
@@ -4,26 +4,32 @@ import { Link } from "react-router-dom"
 
 const ServiceDisplay = ({ service, handleClose }) => {
   return (
-    <div className="Item-container">
-      <p onClick={handleClose} className="close-link" style={{color: "black"}}>
+
+    // <div class="card text-white bg-info mb-3" style="max-width: 20rem;">
+    //   <div class="card-header">
+    //     <p onClick={handleClose} className="close-link" style={{color: "black"}}>X</p>
+    //   </div>
+    //   <div class="card-body">
+    //     <h4 class="card-title">{service.name}</h4>
+    //     <img src={service.images[0]} alt={service.name} style={{height: "50px"}} />
+    //     <p class="card-text">{service.category}</p>
+    //     <p class="card-text">{service.cityName}</p>
+    //     <Link to={`service/${service._id}`} className="btn btn-primary" type="button">Learn more</Link>
+    //   </div>
+    // </div>
+
+
+    <div className="Item-container text-white bg-info mb-3">
+      <p onClick={handleClose} className="close-link mb-0" style={{color: "black"}}>
         X
       </p>
       <div className="round-image">
         <img className="user-img" src={service.images[0]} alt="service" />
       </div>
-      <h2 className="title">{service.name}</h2>
-      <Link to={`service/${service._id}`} className="btn btn-primary" type="button">{service.name}</Link>
-      <div className="info">
-        <span>{service.category}</span>
-      </div>
-      <p className="description">{service.description}</p>
-      <p className="location">{service.cityName}</p>
-      <div className="user-info">
-        <div className="round-image-user">
-          <img src={service.vendorId.profileImg} alt="user" />
-        </div>
-        <span>Provided by <Link to={`vendor/${service.vendorId._id}`} className="btn btn-primary" type="button">{service.vendorId.firstName}</Link></span>
-      </div>
+      <h4 className="card-title mx-auto">{service.name}</h4>
+      <p className="card-text mx-auto">Category: {service.category}</p>
+      <p className="card-text mx-auto">{service.cityName}</p>
+      <Link to={`service/${service._id}`} className="btn btn-primary" type="button">Learn more</Link>
     </div>
   );
 };
